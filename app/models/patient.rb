f334e09180dc2a2f8e4a76286d1b02bd773064f5
armandoteran <ajteranv@gmail.com>
2023-04-30 14:26:49 -0300

    added additional fields in devise registrations new view

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,7 @@ class Patient < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :full_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :state, presence: true
 end
