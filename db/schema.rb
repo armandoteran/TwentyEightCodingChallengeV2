51d41f70a87fa65baba27b17abe8cc20412ff795
armandoteran <ajteranv@gmail.com>
2023-04-30 16:07:30 -0300

    created state model and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_30_173840) do
+ActiveRecord::Schema.define(version: 2023_04_30_190607) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2023_04_30_173840) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "full_name"
+    t.string "abbreviation"
+    t.boolean "service_offered"
+    t.integer "minimum_age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "orders", "patients"
