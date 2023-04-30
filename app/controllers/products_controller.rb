dac591e8fd54071c7fc468e628e947a423c76866
armandoteran <ajteranv@gmail.com>
2023-04-30 15:40:55 -0300

    created show method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_patient!, only: :show
+  def show
+    set_product
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 end
