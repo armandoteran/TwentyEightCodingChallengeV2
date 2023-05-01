3f4cddcd2feccf63a1cf8f69be607afcd0cfef1a
armandoteran <ajteranv@gmail.com>
2023-04-30 22:26:04 -0300

    created order new and create methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,25 @@
 class OrdersController < ApplicationController
+  def new
+    product_set
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @order = Order.new(order_params)
+    @order.patient = current_patient
+    @order.product = @product
+    @order.save
+    redirect_to root_path
+  end
+
+  private
+
+  def product_set
+    @product = Product.find(params[:product_id])
+    @order = Order.new
+  end
+
+  def order_params
+    params.require(:order).permit(:product_id)
+  end
 end
